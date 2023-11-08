10c3f0bf5155c80dc2d42f34b0112e18d0d956fb
David Buzinski <davidbuzinski@gmail.com>
2023-11-08 07:32:40 -0500

    add train.py example for Fashion-MNIST dataset

---
+++ src/main/examples/train.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+# Define model
+class GarmentClassifier(nn.Module):
+    def __init__(self):
+        super(GarmentClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# prep data
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
+training_set = torchvision.datasets.FashionMNIST('./data', train=True, transform=transform, download=True)
+validation_set = torchvision.datasets.FashionMNIST('./data', train=False, transform=transform, download=True)
+training_data = DataLoader(training_set, batch_size=4, shuffle=True)
+validation_data = DataLoader(validation_set, batch_size=4, shuffle=False)
+
+# set key vals
+epochs = 3
+model = [GarmentClassifier()]
+loss = [torch.nn.CrossEntropyLoss()]
+optimizer = [torch.optim.SGD(model[0].parameters(), lr=0.001, momentum=0.9)]
